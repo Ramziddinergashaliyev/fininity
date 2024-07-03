39e86dfe2a1c1224797a2150f2c7d446dc794eca
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-07-03 17:36:34 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Service from "./components/service/Service";
+import Job from "./components/job/Job";
+import Header from "./components/layout/header/Header";
+import Footer from "./components/layout/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import News from "./components/news";
+import Category from "./components/category/Category";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Category />
+      <Job />
+      <Service />
+      <News />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
